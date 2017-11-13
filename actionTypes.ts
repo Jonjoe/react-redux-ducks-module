1b5faebd960b07a96885c9ac279eab7a42e6b86c
Jonjoe Whitfield <me@jonjoe.io>
2017-11-13 01:26:01 +0000

    better starting interfaces and values

--- actionTypes.ts
+++ actionTypes.ts
@@ -6,11 +6,11 @@
 
 // Types ===========================================================
 // -----------------------------------------------------------------
-export const VERB_NOUN = 'VERB_NOUN'
-export type VERB_NOUN = typeof VERB_NOUN;
+export const FETCH_RESOURCE = 'FETCH_RESOURCE'
+export type FETCH_RESOURCE = typeof FETCH_RESOURCE;
 
-export const VERB_NOUN_REJECTED = 'VERB_NOUN_REJECTED'
-export type VERB_NOUN_REJECTED = typeof VERB_NOUN_REJECTED;
+export const FETCH_RESOURCE_REJECTED = 'FETCH_RESOURCE_REJECTED'
+export type FETCH_RESOURCE_REJECTED = typeof FETCH_RESOURCE_REJECTED;
 
-export const VERB_NOUN_FULFILLED = 'VERB_NOUN_FULFILLED'
-export type VERB_NOUN_FULFILLED = typeof VERB_NOUN_FULFILLED;
+export const FETCH_RESOURCE_FULFILLED = 'FETCH_RESOURCE_FULFILLED'
+export type FETCH_RESOURCE_FULFILLED = typeof FETCH_RESOURCE_FULFILLED;
--- reducer.ts
+++ reducer.ts
@@ -6,44 +6,61 @@
 
 // Base Imports ====================================================
 // -----------------------------------------------------------------
-import * as types from './actionTypes'
+import * as types from './actionTypes';
 
-// Definition for inital state =====================================
+// Type Definitions ================================================
 // -----------------------------------------------------------------
-export const INITIAL_STATE = {}
+export interface State {}
+
+export interface CustomError {}
+
+export interface DataPoint {
+  _id: number;
+  __v: number;
+}
+
+export interface Action {
+  type: string;
+  data?: DataPoint[]; 
+  error?: Error; 
+}
+
+// Initial State for reducer =======================================
+// -----------------------------------------------------------------
+export const INITIAL_STATE: State = {};
 
 // Reducer function ================================================
 // -----------------------------------------------------------------
-const reducer = (state = INITIAL_STATE, action) => {
+const reducer = (state: State = INITIAL_STATE, action: Action) => {
   switch (action.type) {
     case types.FETCH_RESOURCE:
       return {
         ...state,
         fetching: true
-      }
+      };
 
     case types.FETCH_RESOURCE_REJECTED:
       return {
         ...state,
         fetching: false,
-        error: action.payload
-      }
+        error: action.error
+      };
 
     case types.FETCH_RESOURCE_FULFILLED:
       return {
         ...state,
         fetching: false,
         fetched: true,
-        collection: action.payload.data
-      }
+        collection: action.data
+      };
 
     default:
-      return state
+      return state;
   }
-}
+};
 
 // Export Definition ===============================================
 // -----------------------------------------------------------------
 export {
   reducer as default
-}
+};
